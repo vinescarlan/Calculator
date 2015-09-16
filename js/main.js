8ad7ca97d0ae85b365ff32cd31b4530017e3bb4a
vinescarlan <vinescarlan@gmail.com>
2015-09-16 17:22:41 +0800

    Create add function and fix some bugs

--- js/main.js
+++ js/main.js
@@ -6,11 +6,14 @@ var prevNum,
 function getValue() {
 	// Check if number count on display is equal to 13
 	//   to prevent overflowing of numbers in display
+	// Note: undefined does not have a length property so we need to check for it
 	if (currentNum === undefined || currentNum.length < 13) {
-		// (currentNum == undefined) == (display == 0)
+		// undefined currentNum == zero (0) display
 		if (currentNum === undefined) {
 			// Prevent calc from displaying a number starting with 0
+			// Info: uses parseInt since the value of input is a string
 			if (parseInt(this.value) === 0) {
+				// Check if display starts with 0
 				if (display.innerHTML.indexOf(0) === 0) {
 					return false; // Prevent the execution of ff. lines of code
 				}
@@ -18,9 +21,14 @@ function getValue() {
 
 			currentNum = this.value;
 			if (parseInt(display.innerHTML) === 0) {
-				// If the display is 0, it will be replace by the button's value
-				//   rather than concatenate to it
-				display.innerHTML = currentNum;
+				// Check for operators in display
+				if (isOperating()) {
+					display.innerHTML += currentNum;
+				} else {
+					// If the display is 0, it will be replace by the button's value
+					//   rather than concatenate to it
+					display.innerHTML = currentNum;
+				}
 			} else {
 				display.innerHTML += currentNum;
 			}
@@ -30,7 +38,9 @@ function getValue() {
 			currentNum += this.value;
 			display.innerHTML += this.value;
 		}
+
 		if (display.innerHTML.length > 18) {
+			// Make the display text smaller so that it will not overflow
 			display.style.fontSize = "16px";
 		}
 	}
@@ -44,40 +54,36 @@ for (var i = 0, len = numberBtns.length; i < len; i++) {
 	numberBtns[i].addEventListener('click', getValue);
 }
 
-/*  When an "operator button (+,-,*,/) is clicked
-		check if the "operator" do not exist on display
-			if yes,
-				set previous num = current num
-				set current num = undefined
-				add "operator" to display (e.g. 1124+)
-				set in use operator = "operator"
-			if no,
-				call "equals" function to display result
-				then add "operator to display"
-			both before, check if current num is 0 -> TODO */
-
 var inuseOperator;
 
-function getOperator() {
+// Check if an operator exist on display
+function isOperating() {
 	var operators = ["+", "-", "*", "/"];
-
-	function checkForOperators() {
-		for (i = 0, len = operators.length; i < len; i++) {
-			if (display.innerHTML.indexOf(operators[i]) == -1) {
-				return true;
-			} else {
-				return false;
-			}
+	for (var i = 0, len = operators.length; i < len; i++) {
+		// If an operator is found
+		if (display.innerHTML.indexOf(operators[i]) !== -1) {
+			return true; // So, the operation will not proceed
+		} else {
+			return false;
 		}
 	}
+}
 
-	if (checkForOperators()) {
-		prevNum = currentNum;
+function getOperator() {
+	if (!isOperating()) {
+		// This will prevent the display from returning NaN if user 
+		// did NOT enter a value || just clicked an operator in the first use
+		if (currentNum === undefined) prevNum = 0;
+		else prevNum = currentNum;
+		// Reset currentNum
 		currentNum = undefined;
+		// Add operator sign in display
 		display.innerHTML += "+";
 		inuseOperator = "+";
 	} else {
-		console.log('false');
+		// TO DO -----> call "equals" function to display result
+		// then add "operator to display"
+		console.log('Operator is already in use');
 	}
 }
 
@@ -85,23 +91,33 @@ var operatorBtns = document.querySelectorAll('.operator');
 
 operatorBtns[3].onclick = getOperator;
 
-/*  When "equals" (=) is clicked or called
-		* check if inuse operator is not undefined to prevent running equal() when
-		* there's no operator in use
-		if yes,
-			check if current no. is undefined
-				if yes,
-					set display = previous num + (inuseoperator)
-								  + previous num
-				if no,
-					set display = previous num + (inuseoperator)
-								  + current num
-				both after,
-					change "delete" button (x) to (=)
-					set previous num = display value
-					set previous num + (operator) + current num
-		if no,
-			return "Can't perform anything, operator is undefined" */
+// When "equals" (=) is clicked or called
+function equal() {
+	// check if inuse operator is not undefined to prevent running equal() when
+	// there's no operator in use
+	if (inuseOperator !== undefined) {
+		// Testing: for checking if display is NaN
+		if (isNaN(parseInt(display.innerHTML))) {
+			console.log(display.innerHTML);
+			display.innerHTML = "Syntax Error";
+		}
+		// check if current no. is undefined
+		// to indicate whether the operation should be repeated
+		if (currentNum === undefined) {
+			display.innerHTML = parseFloat(prevNum) + parseFloat(prevNum);
+		} else {
+			display.innerHTML = parseFloat(prevNum) + parseFloat(currentNum);
+		}
+
+		// TO DO -----> change "delete" button (x) to (=)
+		prevNum = display.innerHTML;
+	} else {
+		console.log("Can't perform anything, operator is undefined");
+	}
+
+}
+
+document.getElementById('equal').onclick = equal;
 
 /*  When "clear all" (AC) is clicked
 		set in use operator = undefined
